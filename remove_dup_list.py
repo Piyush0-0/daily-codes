801af33f6e54e91689037778ccb29631abe2047d
Piyush0-0 <piyushsamanta9885@gmail.com>
2025-07-25 22:25:08 +0530

    remove duplicate lsit

--- remove_dup_list.py
+++ remove_dup_list.py
@@ -2,7 +2,6 @@
 # Given the head of a sorted linked list, delete all duplicates such that 
 # each element appears only once. Return the linked list sorted as well.
 
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
